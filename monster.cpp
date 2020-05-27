3799fa709bb4fb902a922abb753c2fad844e244d
Prawdziwy <damianes7@wp.pl>
2020-05-27 21:45:51 +0200

    CloserSummon implementation in monster

--- monster.cpp
+++ monster.cpp
@@ -250,7 +250,12 @@ void Monster::onCreatureMove(Creature* creature, const Tile* newTile, const Posi
 
 				int32_t offset_x = Position::getDistanceX(followPosition, position);
 				int32_t offset_y = Position::getDistanceY(followPosition, position);
-				if ((offset_x > 1 || offset_y > 1) && mType->info.changeTargetChance > 0) {
+				
+				int16_t offset_check = 1;
+				if (isCloserSummon() == true)
+					offset_check = 0;
+				
+				if ((offset_x > offset_check || offset_y > offset_check) && mType->info.changeTargetChance > 0) {
 					Direction dir = getDirectionTo(position, followPosition);
 					const Position& checkPosition = getNextPosition(dir, position);
 
