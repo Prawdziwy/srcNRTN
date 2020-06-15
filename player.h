aaa9e089538a8d2eb5a545c6fcd46291771ea844
Prawdziwy <damianes7@wp.pl>
2020-06-15 02:21:42 +0200

    Optimize Player::getMuteConditions() by switching to std::vector to reduce cache misses and allocations

--- player.h
+++ player.h
@@ -1060,7 +1060,7 @@ class Player final : public Creature, public Cylinder
 		int32_t burnManaCount;
 		MagicEffectClasses effect;
 
-		std::forward_list<Condition*> getMuteConditions() const;
+		std::vector<Condition*> getMuteConditions() const;
 
 		void checkTradeState(const Item* item);
 		bool hasCapacity(const Item* item, uint32_t count) const;
