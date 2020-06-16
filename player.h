fecf02aaa722f2012a9eb3e8373977607302b57f
Prawdziwy <damianes7@wp.pl>
2020-06-16 04:57:07 +0200

    Optimize Player::invitePartyList

--- player.h
+++ player.h
@@ -1120,7 +1120,7 @@ class Player final : public Creature, public Cylinder
 		std::list<ShopInfo> shopItemList;
 
 		std::unordered_set<std::string> learnedInstantSpellList;
-		std::forward_list<Party*> invitePartyList;
+		std::vector<Party*> invitePartyList;
 		std::forward_list<Condition*> storedConditionList; // TODO: This variable is only temporarily used when logging in, get rid of it somehow
 
 		std::string name;
